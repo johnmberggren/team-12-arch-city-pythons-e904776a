2aeb2e6e94abd3548c1fd0cf2dfa30ee53d6910c
Kelvin <leneil71@outlook.com>
2023-11-16 16:13:11 -0500

    Updated print of character name

--- src/levelup/ui.py
+++ src/levelup/ui.py
@@ -26,7 +26,7 @@ class GameApp:
     def create_character(self):
         character = self.prompt("Enter character name", lambda x: len(x) > 0)
         self.controller.create_character(character)
-        print(f"Welcome, {self.controller.status.character_name}")
+        print(f"Your character name is: {self.controller.status.character_name}")
 
     def move_loop(self):
         while True:
